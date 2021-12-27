76300e9f64985517e2a6ffbd5792e451b0ff76c5
jeshua123 <inv.estevez@gmail.com>
2021-12-26 19:15:17 -0600

    first-commit

--- src/layout/BotomMenu.tsx
+++ src/layout/BotomMenu.tsx
@@ -1,5 +1,35 @@
+import EventIcon from '@mui/icons-material/Event'
+import HistoryIcon from '@mui/icons-material/History'
+import ContactMailIcon from '@mui/icons-material/ContactMail'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import AddIcon from '@mui/icons-material/Add'
 const BotomMenu: React.FC = () => {
-  return <div>Menu mobile</div>
+  return (
+    <>
+      <div className='lg:hidden h-14 bg-white w-full absolute  bottom-0 left-0 rounded-b-2xl flex justify-between items-start  border border-gray-400  '>
+        <button className='bg-white w-2/12 h-5/6 text-gray-900  rounded-bl-2xl pt-2 relative'>
+          <div className='absolute bg-black top-0 h-0.5  w-full'></div>
+          <AccountCircleIcon />
+          <p className='text-xs '>Cuenta</p>
+        </button>
+        <button className='bg-white w-2/12 h-5/6 text-gray-500 pt-2 relative'>
+          <HistoryIcon />
+          <p className='text-xs'>Historial</p>
+        </button>
+        <button className='bg-white w-2/12 h-5/6 text-gray-500 pt-2 relative'>
+          <ContactMailIcon />
+          <p className='text-xs'>Roles</p>
+        </button>
+        <button className='bg-white w-2/12  h-5/6 text-gray-500 pt-2 relative'>
+          <EventIcon />
+          <p className='text-xs '>Evento</p>
+        </button>
+        <button className='w-2/12 rounded-br-2xl h-5/6  text-green-400 pt-2 relative'>
+          <AddIcon />
+          <p className='text-xs'>Agregar</p>
+        </button>
+      </div>
+    </>
+  )
 }
-
 export default BotomMenu
--- src/layout/CpanelLayout.tsx
+++ src/layout/CpanelLayout.tsx
@@ -1,17 +1,18 @@
 import SideMenu from './SideMenu'
+import BotomMenu from './BotomMenu'
 import { Outlet } from 'react-router-dom'
 
 const CpanelLayout: React.FC = () => {
   return (
-    <div className='grid grid-cols-12 h-screen overflow-hidden bg-green-400 md:p-8 p-4 gap-x-4'>
+    <div className='grid grid-cols-12 h-screen overflow-hidden bg-green-400 md:p-8 p-4 gap-x-4  relative '>
       <div className='col-span-2 h-full boder border-black rounded-2xl p-4 bg-white shadow-lg lg:block hidden'>
         <SideMenu />
       </div>
-      <div className='lg:col-span-10 col-span-12 h-full border-black rounded-2xl md:p-8 p-4 bg-white shadow-lg'>
+      <div className='lg:col-span-10 col-span-12 h-full border-black rounded-2xl md:p-8 p-4 bg-white shadow-lg relative'>
         <Outlet />
+        <BotomMenu />
       </div>
     </div>
   )
 }
-
 export default CpanelLayout
